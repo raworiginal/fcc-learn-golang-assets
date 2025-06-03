9d2aae13ce8a3c0f039aee0246b8943e6e047fe7
raworiginal <118763950+raworiginal@users.noreply.github.com>
2025-06-03 11:14:04 -0700

    completing exercises chapter 1

--- course/1-intro/exercises/5-compiling_code/complete.go
+++ course/1-intro/exercises/5-compiling_code/complete.go
@@ -1,7 +1,7 @@
-package main
+// package main
 
-import "fmt"
+// import "fmt"
 
-func main() {
-	fmt.Println("the-compiled Textio server is starting")
-}
+// // func main() {
+// // 	fmt.Println("the-compiled Textio server is starting")
+// // }
--- course/1-intro/exercises/8-strongly_typed/complete.go
+++ course/1-intro/exercises/8-strongly_typed/complete.go
@@ -1,11 +1,11 @@
-package main
+// package main
 
-import "fmt"
+// import "fmt"
 
-func main() {
-	var username string = "wagslane"
-	var password string = "20947382822"
+// func main() {
+// 	var username string = "wagslane"
+// 	var password string = "20947382822"
 
-	// don't edit below this line
-	fmt.Println("Authorization: Basic", username+":"+password)
-}
+// 	// don't edit below this line
+// 	fmt.Println("Authorization: Basic", username+":"+password)
+// }
