26b6d2368e5a4139d7c9b25cac50edc014e1601c
raworiginal <118763950+raworiginal@users.noreply.github.com>
2025-06-09 09:44:35 -0700

    finished chapter 6 errors

--- course/6-errors/exercises/3-custom_errors/code.go
+++ course/6-errors/exercises/3-custom_errors/code.go
@@ -8,7 +8,11 @@ type divideError struct {
 	dividend float64
 }
 
-// ?
+func (de divideError) Error() string {
+	return fmt.Sprintf("cannot divide %v by zero",
+		de.dividend,
+	)
+}
 
 // don't edit below this line
 
